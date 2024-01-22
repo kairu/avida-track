e73b43e02f4ba09e2d2df9abce7065a9122acfd9
kairu <kairugaming@gmail.com>
2024-01-23 04:25:56 +0800

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  CommonModule, LocationStrategy,
+  PathLocationStrategy
+} from '@angular/common';
+import { NgModule, isDevMode } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { FullComponent } from './layouts/full/full.component';
+
+
+import { NavigationComponent } from './shared/header/navigation.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+import { Approutes } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SpinnerComponent } from './shared/spinner.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { ManageUsersComponent } from './component/manage-users/manage-users.component';
+// import { ManageUsersComponent } from './component/manage-users/manage-users.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SpinnerComponent,
+    ManageUsersComponent,
+    // ManageUsersComponent,
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule,
+    RouterModule.forRoot(Approutes, { useHash: false}),
+    FullComponent,
+    NavigationComponent,
+    SidebarComponent,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+  ],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
