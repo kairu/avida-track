3ed11f697bf2fbecf27743aa79a64083cedd120c
jaaayz <tyrax_mere@yahoo.com>
2024-06-20 11:51:16 +0800

    Start of overhauling tenant-lease module

--- src/app/component/tenant-lease/tenant-lease.component.ts
+++ src/app/component/tenant-lease/tenant-lease.component.ts
@@ -9,16 +9,39 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { FormsModule } from '@angular/forms';
 import { ImageModule } from 'primeng/image';
 import { CalendarModule } from 'primeng/calendar';
-
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { DialogModule } from 'primeng/dialog';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 interface UploadEvent {
   originalEvent: Event;
   files: File[];
 }
 
+interface AutoCompleteCompleteEvent {
+  originalEvent: Event;
+  query: string;
+}
+interface Tenant {
+  user_id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  mobile_number: string;
+  user_type: string;
+  monthly_rent?: number;
+  number_of_months?: number;
+  contract?: string;
+  start_date?: string;
+  end_date?: string;
+  remaining_balance?: number;
+  fullName?: string;
+}
 @Component({
   selector: 'app-tenant-lease',
   standalone: true,
-  imports: [TableModule, FileUploadModule, CommonModule, InputNumberModule, FormsModule, ImageModule, CalendarModule],
+  imports: [TableModule, FileUploadModule, CommonModule, InputNumberModule, FormsModule, 
+            ImageModule, CalendarModule, ButtonModule, RippleModule, DialogModule, AutoCompleteModule],
   templateUrl: './tenant-lease.component.html',
   styleUrls: ['./tenant-lease.component.scss']
 })
@@ -34,15 +57,71 @@ export class TenantLeaseComponent implements OnInit {
   today: Date;
   numberOfMonths: number | null = null;
   computedEndDates: { [leaseId: number]: string } = {};
-
-
+  visible: boolean = false;
+  // items: any[] | undefined;
+  // suggestions: any[] = [];
+  // selectedTenants: any = null;
+  // selectedTenant: any;
+  tenant: Tenant[] = [];
+  suggestions: Tenant[] = [];
+  selectedTenants: Tenant[] = [];
+  selectedTenant: Tenant | null = null; 
 
   constructor(private backendService: BackendServiceService, private messageService: MessageService) {
     this.today = new Date();
+   
   }
 
+  showDialog() {
+    this.visible = true;
+  }
+
+  search(event: AutoCompleteCompleteEvent) {
+    const tenant = event.query;
+    console.log('Search query:', tenant);
+    this.backendService.getUsers().subscribe({
+      next: (response: any) => {
+        let tenants = [];
+        if (Array.isArray(response)) {
+          tenants = response;
+        }
+        const filteredTenants = tenants.filter((user: { user_type: string }) => {
+          return user.user_type === 'TENANT';
+        });
+
+        filteredTenants.forEach((tenant: { first_name: string; last_name: string; fullName?: string }) => {
+          tenant.fullName = `${tenant.first_name} ${tenant.last_name}`;
+        });
+
+        console.log('Filtered tenant data:', filteredTenants);
+        this.suggestions = filteredTenants.filter((user: { first_name: string; last_name: string; fullName: string }) =>
+          user.fullName.toLowerCase().includes(tenant.toLowerCase())
+        );
+
+        console.log('Filtered suggestions:', this.suggestions);
+      },
+      error: (error) => {
+        console.error('Error fetching tenants:', error);
+        this.suggestions = [];
+      }
+    });
+}
+
+onTenantSelected(event: any) {
+  const selectedTenant: Tenant = event.value;
+  console.log('Tenant selected:', selectedTenant);
+
+  if (!this.selectedTenants.some(tenant => tenant.user_id === selectedTenant.user_id)) {
+    this.selectedTenants.push(selectedTenant);
+  }
+
+  this.selectedTenant = null;
+}
+
+
+
   ngOnInit(): void {
-    this.getLease();
+    // this.getLease();
     const storedLeases = localStorage.getItem('leases');
     if (storedLeases) {
       this.leases = JSON.parse(storedLeases);
@@ -52,72 +131,64 @@ export class TenantLeaseComponent implements OnInit {
       
     }
     this.leases.forEach(lease => lease.numberOfMonths = 1);
-    // this.leases.forEach(lease => {
-    //   lease.numberOfMonths = JSON.parse(localStorage.getItem(`numberOfMonths_${lease.lease_agreement_id}`) || '1');
-    //   const storedStartDate = localStorage.getItem(`startDate_${lease.lease_agreement_id}`);
-    //   lease.start_date = storedStartDate ? JSON.parse(storedStartDate) : lease.start_date;
-
-    //   const storedEndDate = localStorage.getItem(`endDate_${lease.lease_agreement_id}`);
-    //   lease.end_date = storedEndDate ? JSON.parse(storedEndDate) : lease.end_date;
-    // }); 
   }
 
-  getLease(): void {
-    const email = this.backendService.getEmail();
-    this.backendService.getUser(email).subscribe({
-      next: (userData) => {
-        const leases: any[] = [];
+  // getLease(): void {
+  //   const email = this.backendService.getEmail();
+  //   this.backendService.getUser(email).subscribe({
+  //     next: (userData) => {
+  //       const leases: any[] = [];
 
-        if (userData.lease_agreements && Array.isArray(userData.lease_agreements)) {
-          userData.lease_agreements.forEach((leaseAgreement: any) => {
-            const tenantEmail = leaseAgreement.tenant_info.email;
-            this.backendService.getUser(tenantEmail).subscribe({
-              next: (tenantData) => {
-                const leaseInfo = {
-                  lease_agreement_id: leaseAgreement.lease_agreement_id,
-                  unit_id: leaseAgreement.unit_id,
-                  owner_id: leaseAgreement.owner_id,
-                  tenant_id: leaseAgreement.tenant_id,
-                  start_date: leaseAgreement.start_date,
-                  end_date: leaseAgreement.end_date,
-                  monthly_rent: leaseAgreement.monthly_rent,
-                  remaining_balance: leaseAgreement.remaining_balance,
-                  security_deposit: leaseAgreement.security_deposit,
-                  contract: leaseAgreement.contract,
-                  tenant_info: {
-                    first_name: tenantData.first_name,
-                    last_name: tenantData.last_name,
-                    email: tenantData.email,
-                  },
-                  startDateSaved: false,
-                  disabledInput: false,
-                  updatedMonthlyRent: null, // Initialize with the current monthly rent
-                };
-                leases.push(leaseInfo);
-                console.log('Lease added:', leaseInfo);
-              },
-              error: (error) => {
-                console.error('Error fetching tenant data:', error);
-              }
-            });
-          });
-        } else {
-          console.error('Lease agreements data not found or invalid.');
-        }
-        this.leases = leases;
+  //       if (userData.lease_agreements && Array.isArray(userData.lease_agreements)) {
+  //         userData.lease_agreements.forEach((leaseAgreement: any) => {
+  //           const tenantEmail = leaseAgreement.tenant_info.email;
+  //           this.backendService.getUser(tenantEmail).subscribe({
+  //             next: (tenantData) => {
+  //               const leaseInfo = {
+  //                 lease_agreement_id: leaseAgreement.lease_agreement_id,
+  //                 unit_id: leaseAgreement.unit_id,
+  //                 owner_id: leaseAgreement.owner_id,
+  //                 tenant_id: leaseAgreement.tenant_id,
+  //                 start_date: leaseAgreement.start_date,
+  //                 end_date: leaseAgreement.end_date,
+  //                 monthly_rent: leaseAgreement.monthly_rent,
+  //                 remaining_balance: leaseAgreement.remaining_balance,
+  //                 security_deposit: leaseAgreement.security_deposit,
+  //                 contract: leaseAgreement.contract,
+  //                 tenant_info: {
+  //                   first_name: tenantData.first_name,
+  //                   last_name: tenantData.last_name,
+  //                   email: tenantData.email,
+  //                 },
+  //                 startDateSaved: false,
+  //                 disabledInput: false,
+  //                 updatedMonthlyRent: null, // Initialize with the current monthly rent
+  //               };
+  //               leases.push(leaseInfo);
+  //               console.log('Lease added:', leaseInfo);
+  //             },
+  //             error: (error) => {
+  //               console.error('Error fetching tenant data:', error);
+  //             }
+  //           });
+  //         });
+  //       } else {
+  //         console.error('Lease agreements data not found or invalid.');
+  //       }
+  //       this.leases = leases;
 
-        if (this.leases.length > 0) {
-          this.selectedItem = this.leases[0];
-          console.log(this.selectedItem);
-        } else {
+  //       if (this.leases.length > 0) {
+  //         this.selectedItem = this.leases[0];
+  //         console.log(this.selectedItem);
+  //       } else {
           
-        }
-      },
-      error: (error) => {
-        console.error('Error fetching user data:', error);
-      }
-    });
-  }
+  //       }
+  //     },
+  //     error: (error) => {
+  //       console.error('Error fetching user data:', error);
+  //     }
+  //   });
+  // }
 
   async storeImageData(event: any, lease_agreement_id: number) {
     console.log(lease_agreement_id);
@@ -250,12 +321,6 @@ export class TenantLeaseComponent implements OnInit {
           this.messageService.add({ severity: 'success', summary: 'Lease Dates Updated', detail: 'Start and end dates updated successfully.' });
           lease.startDateSaved = true; // Set the startDateSaved flag to true
           localStorage.setItem('leases', JSON.stringify(this.leases));
-          // To set the numberOfMonths in local storage
-          // localStorage.setItem(`numberOfMonths_${lease.lease_agreement_id}`, JSON.stringify(lease.numberOfMonths));
-          // localStorage.setItem(`startDate_${lease.lease_agreement_id}`, JSON.stringify(lease.start_date));
-          // localStorage.setItem(`endDate_${lease.lease_agreement_id}`, JSON.stringify(lease.end_date));
-
-          
         },
         error: (error) => {
           console.error('Error updating lease dates:', error);
